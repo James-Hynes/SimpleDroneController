90f7003f0d684d92494aeca3f0b2c264c73a321d
James R. Hynes <james.hynes@sonomaacademy.org>
2016-09-18 15:10:43 -0700

    Removed binary

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ let DroneController = function() {
 
   this.getActionDescription = function(k, drone) {
     let actions = {'up': 'Moving forward', 'down': 'Moving backwards', 'right': 'Tilting right', 'left': 'Tilting left', 'w': 'Moving up', 's': 'Moving down', 
-    'a': 'Turning left', 'd': 'Turning right', 'f': 'Performing front flip', 'b': 'Performing back flip', 'n': 'Taking off', 'p': 'Landing', 'x': 'Exiting program...', 
+    'a': 'Turning left', 'd': 'Turning right', 'p': 'Performing front flip', 'b': 'Performing back flip', 'n': 'Taking off', 'f': 'Landing', 'x': 'Exiting program...', 
     'z': ((k.ctrl || k.shift) ? ((drone.connected) ? 'Disconnecting': 'Connecting') : 'Hold shift and press Z to connect/disconnect')};
     return ((k.name in actions) ? actions[k.name] : 'no action');
   }
@@ -39,22 +39,22 @@ let DroneController = function() {
     if(k) {
       console.log(this.getActionDescription(k, d));
       switch(k.name) {
-        case this.translateBinary('01110101 01110000'): d.forward({steps: 0b11, speed: 0b1100100}); break;
-        case this.translateBinary('01100100 01101111 01110111 01101110'): d.backward({steps: 0b101, speed: 0b1100100}); break;
-        case this.translateBinary('01110010 01101001 01100111 01101000 01110100'): d.tiltRight({steps: 0b101, speed: 0b1100100}); break;
-        case this.translateBinary('01101100 01100101 01100110 01110100'): d.tiltLeft({steps: 0b11, speed: 0b1100100}); break;
-        case this.translateBinary('01110111'): d.up({steps: 0b101, speed: 0b1100100}); break;
-        case this.translateBinary('01110011'): d.down({steps: 0b101, speed: 0b1100100}); break;
-        case this.translateBinary('01100001'): d.turnLeft({steps: 0b101, speed: 0b1100100}); break;
-        case this.translateBinary('01100100'): d.turnRight({steps: 0b101, speed: 0b1100100}); break;
-        case this.translateBinary('01110000'): d.frontFlip(); break;
-        case this.translateBinary('01100010'): d.backFlip(); break;
-        case this.translateBinary('01101100'): d.leftFlip(); break;
-        case this.translateBinary('01110010'): d.rightFlip(); break;
-        case this.translateBinary('01101110'): d.flatTrim(); (((process.argv.slice(2)[0] === true || process.argv.slice(2)[0] === 'true')) ? d.wheelOn() : d.wheelOff()); d.takeOff(); break;
-        case this.translateBinary('01100110'): d.land(); break;
-        case this.translateBinary('01111000'): d.land(); d.disconnect(); process.stdin.pause(); process.exit(0); break;
-        case this.translateBinary('01111010'): if(k.ctrl || k.shift) { if(d.connected) { d.disconnect(); }  else {this.connectDrone(d); } } break;
+        case 'up': d.forward({steps: 5, speed: 100}); break;
+        case 'down': d.backward({steps: 5, speed: 100}); break;
+        case 'right': d.tiltRight({steps: 5, speed: 100}); break;
+        case 'left': d.tiltLeft({steps: 5, speed: 100}); break;
+        case 'w': d.up({steps: 5, speed: 100}); break;
+        case 's': d.down({steps: 5, speed: 100}); break;
+        case 'a': d.turnLeft({steps: 5, speed: 100}); break;
+        case 'd': d.turnRight({steps: 5, speed: 100}); break;
+        case 'p': d.frontFlip(); break;
+        case 'b': d.backFlip(); break;
+        case 'l': d.leftFlip(); break;
+        case 'r': d.rightFlip(); break;
+        case 'n': d.flatTrim(); (((process.argv.slice(2)[0] === true || process.argv.slice(2)[0] === 'true')) ? d.wheelOn() : d.wheelOff()); d.takeOff(); break;
+        case 'f': d.land(); break;
+        case 'x': d.land(); d.disconnect(); process.stdin.pause(); process.exit(0); break;
+        case 'z': if(k.ctrl || k.shift) { if(d.connected) { d.disconnect(); }  else {this.connectDrone(d); } } break;
         default: console.log(`${k.name} not mapped`);
       }
     }
